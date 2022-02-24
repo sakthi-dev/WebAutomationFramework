55b8a9ba9c4f516dd36f2ed659ffdbb233a98c51
sakthi-saravana-ext <ssakthisaravana-ext@drivewealth.tech>
2022-02-25 01:57:42 +0530

    added and update a code

--- src/main/java/com/qa/driveclear/utils/Constants.java
+++ src/main/java/com/qa/driveclear/utils/Constants.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Constants {
 
-	public static final String LOGIN_PAGE_TITLE = "Account Loginn";
+	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String LOGIN_PAGE_URL_FRACTION = "account/login";
 	public static final int DEFAULT_TIME_OUT = 5;
 	public static final String ACCOUNTS_PAGE_TITLE = "My Account";
